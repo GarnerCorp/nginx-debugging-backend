a466a2bdd74b1b3557f2c0aea17b1a582eefd78b
Josh Soref <jsoref@users.noreply.github.com>
2022-03-10 14:58:26 -0500

    Add support for ingress-nginx custom-http-errors
    https://kubernetes.github.io/ingress-nginx/user-guide/nginx-configuration/configmap/#custom-http-errors

--- debug.py
+++ debug.py
@@ -15,8 +15,45 @@ app = Flask(__name__)
 def healthz():
     return 'healthy\n'
 
+def special_responses():
+    code = request.headers['X-Code']
+    code_str = str(code)
+    try:
+        kind = http.HTTPStatus(code)
+    except ValueError:
+        kind = "HTTP Error " + code_str
+
+    response = [
+        '<title>' + kind + '</title>',
+        '<h1>' + kind + '</h1>',
+        '<strong>' + code_str + '</strong> That’s an error.'
+        '<p>',
+        'We’re sorry.',
+        '</p>',
+        '<p>Please try again later.',
+        '<em>That’s all we know.</em>',
+        '</p>',
+        ''
+    ]
+    x = [
+        kind,
+        ''
+    ]
+    x.append("%s %s" % (request.method, request.url))
+    x.append('')
+    for a in request.headers:
+        x.append("%s: %s" % (a[0], a[1]))
+    log = " -- ".join(x)
+    data = request.get_data()
+    if data:
+        log += " ---- " + urllib.parse.quote_plus(data)
+    sys.stderr.write(log + "\n")
+    return Response(response="\n".join(x), status=code, mimetype="text/html")
+
 @app.route('/')
 def index():
+    if 'X-Code' in request.headers:
+        return special_responses()
     x = [
       'Request Debugging Information',
       'Request time: %s' % datetime.now().isoformat(),
